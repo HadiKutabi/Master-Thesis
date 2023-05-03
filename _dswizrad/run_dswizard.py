fdd9e8bec4144331965bf52d1499f7c4d9accd4d
Hadi Kutabi <hadi.kutabi@tu-dortmund.de>
2023-05-03 17:37:48 +0200

    collecting stats in dswizard

--- _dswizrad/run_dswizard.py
+++ _dswizrad/run_dswizard.py
@@ -28,8 +28,10 @@ def fetch_and_get_dataset_ready(ds):
     X_train, X_test = train.drop("target", axis=1), test.drop("target", axis=1)
     y_train, y_test = train[["target"]], test[["target"]],
 
-    train_ds = Dataset(X_train.values, y_train.values.ravel(), metric="accuracy", task=123, fold=0, feature_names=list(X_train.columns))
-    test_ds = Dataset(X_test.values, y_test.values.ravel(), metric="accuracy", task=123, fold=0, feature_names=list(X_train.columns))
+    train_ds = Dataset(X_train.values, y_train.values.ravel(), metric="accuracy", task=123, fold=0,
+                       feature_names=list(X_train.columns))
+    test_ds = Dataset(X_test.values, y_test.values.ravel(), metric="accuracy", task=123, fold=0,
+                      feature_names=list(X_train.columns))
 
     return train_ds, test_ds
 
@@ -43,19 +45,58 @@ def fetch_and_get_dataset_ready(ds):
 #     stats.to_csv(pjoin(save_to_dir, "run_stats.csv"), index=False)
 
 
-def predict_and_save(aml, x_test, save_to_dir):
-    pred = aml.predict(x_test)
-    pred_proba = aml.predict_proba(x_test)
+# def predict_and_save(aml, x_test, save_to_dir):
+#     pred = aml.predict(x_test)
+#     pred_proba = aml.predict_proba(x_test)
+#
+#     pred_df = pd.DataFrame()
+#
+#     pred_df["pred"] = pred
+#     pred_df["pred_proba_0"] = pred_proba[:, 0]
+#     pred_df["pred_proba_1"] = pred_proba[:, 1]
+#
+#     pred_df.to_csv(pjoin(save_to_dir, "prediction.csv"), index=False)
+#
+#     return pred, pred_proba
 
-    pred_df = pd.DataFrame()
 
-    pred_df["pred"] = pred
-    pred_df["pred_proba_0"] = pred_proba[:, 0]
-    pred_df["pred_proba_1"] = pred_proba[:, 1]
+def make_predictions(pipeline, ensemble, test_ds, save_to_dir):
+    preds = pd.DataFrame()
 
-    pred_df.to_csv(pjoin(save_to_dir, "prediction.csv"), index=False)
+    pipe_pred = pipeline.predict(test_ds.X)
+    pipe_pred_proba = pipeline.predict_proba(test_ds.X)
+    preds["inc_pred"] = pipe_pred
+    for i in range(0, pipe_pred_proba.shape[1]):
+        preds[f"inc_pred_proba_{i}"] = pipe_pred_proba[:, i]
 
-    return pred, pred_proba
+    ensemble_pred = ensemble.predict(test_ds.X)
+    ensemble_pred_proba = ensemble.predict_proba(test_ds.X)
+    preds["ens_pred"] = ensemble_pred
+    for i in range(0, ensemble_pred_proba.shape[1]):
+        preds[f"ens_pred_proba_{i}"] = ensemble_pred_proba[:, i]
+
+    preds.to_csv(pjoin(save_to_dir, "predictions.csv"), index=False)
+
+    return preds
+
+
+def compute_and_save_score(pred, test_ds, train_score, save_to_dir):
+    df_perf = pd.DataFrame()
+    df_perf["train_accuracy"] = train_score
+    print(accuracy_score(test_ds.y, pred["inc_pred"].values.ravel()))
+    df_perf["incumbent_test_score"] = accuracy_score(test_ds.y, pred["inc_pred"].values.ravel())
+    df_perf["ensemble_test_score"] = accuracy_score(test_ds.y, pred["ens_pred"].values.ravel())
+    df_perf.to_csv(pjoin(save_to_dir, "scores.csv"), index=False)
+
+
+def compute_and_save_score(pred, test_ds, train_score, save_to_dir):
+    data = {}
+    data["train_score"] = [train_score]
+    data["incumbent_test_score"] = [accuracy_score(test_ds.y, pred["inc_pred"].values.ravel())]
+    data["ensemble_test_score"] = [accuracy_score(test_ds.y, pred["ens_pred"].values.ravel())]
+
+    df_perf = pd.DataFrame.from_dict(data)
+    df_perf.to_csv(pjoin(save_to_dir, "scores.csv"), index=False)
 
 
 def main():
@@ -73,13 +114,11 @@ def main():
 
     train_ds, test_ds = fetch_and_get_dataset_ready(DS)
 
-
     np.random.seed(SEED)
 
     automl_params["working_directory"] = RUN_DIR
     automl_params["ds"] = train_ds
     automl_params["logger"] = logger
-
 
     print("Starting AutoML procedure")
 
@@ -91,6 +130,14 @@ def main():
 
     print(f"DSWIZARD took {total_time}")
 
+    _, incumbent = run_history.get_incumbent()
+    dump_pickle(pipeline, pjoin(RUN_DIR, "pipeline.pkl"))
+    dump_pickle(ensemble, pjoin(RUN_DIR, "ensemble.pkl"))
+    dump_pickle(run_history, pjoin(RUN_DIR, "my_run_history.pkl"))
+    dump_pickle(incumbent, pjoin(RUN_DIR, "incumbent.pkl"))
+
+    pred_df = make_predictions(pipeline, ensemble, test_ds, RUN_DIR)
+    compute_and_save_score(pred_df, test_ds, -incumbent.get_incumbent().loss, RUN_DIR)
 
 
 if __name__ == "__main__":
@@ -104,4 +151,3 @@ if __name__ == "__main__":
         args.dataset = d
         print(args.dataset)
         main()
-
